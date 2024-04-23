12213ca512671c64cebc9b47f802f6f971797fa8
Minal <94733406+minal-95@users.noreply.github.com>
2024-04-23 13:13:14 +0530

    Update Exp8_MiniMax.py

--- Exp8_MiniMax.py
+++ Exp8_MiniMax.py
@@ -1,1 +1,42 @@
+def minimax(depth, nodeIndex, isMax, scores, alpha, beta):
+    # Terminating condition. i.e. leaf node is reached
+    if depth == 3:
+        return scores[nodeIndex]
+
+    if isMax:
+        best = -float('inf')
+
+        # Recur for left and right children
+        for i in range(0, 2):
+            val = minimax(depth + 1, nodeIndex * 2 + i, False, scores, alpha, beta)
+            best = max(best, val)
+            alpha = max(alpha, best)
+
+            # Alpha Beta Pruning
+            if beta <= alpha:
+                break
+
+        return best
+
+    else:
+        best = float('inf')
+
+        # Recur for left and right children
+        for i in range(0, 2):
+            val = minimax(depth + 1, nodeIndex * 2 + i, True, scores, alpha, beta)
+            best = min(best, val)
+            beta = min(beta, best)
+
+            # Alpha Beta Pruning
+            if beta <= alpha:
+                break
+
+        return best
+
+# Driver code
+scores = [3, 5, 6, 9, 1, 2, 0, -1] # Assuming a specific tree leaf values
+alpha = -float('inf')
+beta = float('inf')
+bestVal = minimax(0, 0, True, scores, alpha, beta)
+print("The optimal value is :", bestVal)
 
